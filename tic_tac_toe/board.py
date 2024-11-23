1819a19b2952ffb6f0b336498b0ba79d04079f06
Stephen Lalor <stephen.lalor.work@gmail.com>
2024-11-23 20:13:04 +0000

    Cleanup, removed unused initialise method, added next player as well as last player for convenience.

--- tic_tac_toe/board.py
+++ tic_tac_toe/board.py
@@ -9,33 +9,24 @@ class TicTacToeException(Exception):
 
 class TicTacToeBoard:
     def __init__(self, p1: TicTacToeBot, p2: TicTacToeBot):
+        self.dim = (3, 3)
+        self.state = np.zeros(self.dim, dtype=np.int8)
+        self.valid_moves = [loc for loc in np.ndindex(self.state.shape)]
         self.p1 = p1
         self.p2 = p2
-        self.last_player = None
-        self.dim = None
-        self.state = None
-        self.valid_moves = None
+        self.next_player = p1
+        self.last_player = p2
         self.last_move = None
         self.game_result = None
-        self.init_board()
 
     def __str__(self):
         if self.state is None:
             return ""
-        lst = self.state.tolist()
-        parsed_board = [[f"[{x.decode() if x.decode() else " "}]" for x in row] for row in lst]
-        formatted_board = "\n".join([" ".join(row) for row in parsed_board])
-        header_msg = f"Board State (moves: {len(self.valid_moves)}, status: {str(self.game_result)})"
-        final_msg = f"{header_msg}\n{formatted_board}"
-        return final_msg
-
-    def init_board(self):
-        self.dim = (3, 3)
-        self.state = np.zeros(self.dim, dtype="S1")
-        self.valid_moves = [loc for loc in np.ndindex(self.state.shape)]  # List as we will make random selection later.
-        self.game_result = None
-        self.last_player = None
-        self.last_move = None
+        formatted_board = "\n".join(["".join(str(row)) for row in self.state.tolist()])
+        header_msg = (
+            f"Board State (moves: {len(self.valid_moves)}, status: {str(self.game_result)})"
+        )
+        return f"{header_msg}\n{formatted_board}"
 
     def update_board(self, symbol, move_loc):
         self.state[move_loc] = symbol
@@ -56,12 +47,14 @@ class TicTacToeBoard:
 
         # Check for diagonal win.
         row_diag, col_diag = zip(*[(0 + i, 0 + i) for i in range(self.dim[0])])
-        # Co-ords for diagonal and ancy indexing to avoid flipping state.
-        diag = self.state[list(row_diag), list(col_diag)]
         row_adiag, col_adiag = zip(*[(0 + i, self.dim[1] - 1 - i) for i in range(self.dim[1])])
-        # Co-ords for anti-diagonal and fancy indexing.
-        anti_diag = self.state[list(row_adiag), list(col_adiag)]
-        if anti_diag[0] and np.all(anti_diag == anti_diag[0]) or (diag[0] and np.all(diag == diag[0])):
+        # Co-ords for diagonal and fancy indexing to avoid flipping state.
+        diag = self.state[list(row_diag), list(col_diag)]
+        adiag = self.state[list(row_adiag), list(col_adiag)]
+        # Check for winning state.
+        adiag_win = adiag[0] and np.all(adiag == adiag[0])
+        diag_win = diag[0] and np.all(diag == diag[0])
+        if adiag_win or diag_win:
             self.game_result = "win"
             return
 
@@ -70,13 +63,9 @@ class TicTacToeBoard:
             self.game_result = "draw"
         return
 
-    def get_next_player(self):
-        return self.p1 if self.last_player in [self.p2, None] else self.p2
-
     def exec_move(self, move_loc):
-        player = self.get_next_player()  # We need the right symbol.
-        self.update_board(player.symbol, move_loc)
-        self.last_player = player
+        self.update_board(self.next_player.symbol, move_loc)
+        self.next_player, self.last_player = self.last_player, self.next_player
         self.last_move = move_loc
         self.update_result()
         return
