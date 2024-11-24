a654aeb26e0bfa6bc880adc6810f457e6a55cba6
Stephen Lalor <stephen.lalor.work@gmail.com>
2024-11-24 23:08:22 +0000

    Renamed brute force MCTS class as the one using the NN is quite different

---
+++ mcts/brute_mcts.py
@@ -0,0 +1,129 @@
+import copy
+
+import numpy as np
+
+from tic_tac_toe.board import TicTacToeBoard, TicTacToeException
+from tic_tac_toe.player import TicTacToeBot
+
+
+class BruteMCSTNode:
+    """
+    Monte Carlo Search Tree Node.
+    """
+
+    def __init__(self, parent: "BruteMCSTNode", board: TicTacToeBoard, opts: dict):
+        self.parent = parent
+        self.children = []
+        self.opt_child = None
+        self.board = board
+        self.unused_moves = board.valid_moves.copy()
+        self.visits = 0
+        self.value = 0
+        self.opts = opts
+
+    def __str__(self):
+        has_parent = self.parent is not None
+        last_player, last_move = self.board.last_player, self.board.last_move
+        struct_info = f"[Parent] {has_parent} [Children] {len(self.children)}"
+        last_info = f"[Last Player] {last_player} [Last Move] {last_move}"
+        uct_info = f"[Visits] {self.visits} [Value] {self.value}"
+        return f"{struct_info} | {uct_info} | {last_info}"
+
+    def calc_utc_score(self, child: "BruteMCSTNode", c: float) -> float:
+        """
+        Calculate UTC score for child WRT parent node.
+        """
+        exploitation = child.value / child.visits
+        exploration = np.sqrt(np.log(self.visits) / child.visits)
+        return exploitation + (c * exploration)
+
+    def get_best_child(self, c: float) -> "BruteMCSTNode":
+        """
+        Choose child with highest UCT score.
+        """
+        best_child_node, best_score = None, -np.inf
+        for child in self.children:
+            score = self.calc_utc_score(child, c)
+            if score > best_score:
+                best_score, best_child_node = score, child
+        if best_child_node is None:
+            raise ValueError("Failed to find best child node.")
+        return best_child_node
+
+    def expand(self) -> "BruteMCSTNode":
+        """
+        Add a child node corresponding to new game state after an unused move has been executed.
+        """
+        new_board = copy.deepcopy(self.board)
+        # Exec next available move for child, not random move.
+        new_board.exec_move(self.unused_moves.pop())
+        child = BruteMCSTNode(self, new_board, self.opts)
+        self.children.append(child)
+        return child  # Return the expanded child node.
+
+    def is_fully_expanded(self) -> bool:
+        """
+        Node is fully expanded if there are no moves remaining that have not already been tried.
+        """
+        return len(self.unused_moves) == 0
+
+    def traverse(self) -> "BruteMCSTNode":
+        """
+        Step through best child nodes if fully expanded, or expand if not.
+        """
+        node = self  # Start at this current node.
+        while not node.board.game_result:
+            if not node.is_fully_expanded():
+                return node.expand()
+            node = node.get_best_child(self.opts["c"])
+        return node
+
+    def rollout(self) -> TicTacToeBot | str:
+        """
+        From the current state, play to the end to get a result.
+        """
+        board = copy.deepcopy(self.board)
+        while not board.game_result:
+            # Directly use player's move generation (now random move).
+            player = board.get_next_player()
+            move = player.generate_move(board.valid_moves)
+            board.exec_move(move[0])
+            if board.game_result:
+                return player if board.game_result == "win" else "draw"
+        return board.game_result
+
+    def get_reward(self, node: "BruteMCSTNode", game_result: TicTacToeBot | str) -> float:
+        """
+        Calculate reward for a terminated game.
+        """
+        if game_result == "draw":
+            return self.opts["draw"]
+        if node.board.last_player == game_result:
+            return self.opts["win"]
+        if node.board.last_player != game_result:
+            return self.opts["lose"]
+        err_msg = f"Game result {game_result} not one of {[node.board.p1, node.board.p2, "draw"]}"
+        raise TicTacToeException(err_msg)
+
+    def backpropagate(self, game_result: TicTacToeBot | str):
+        """
+        From current node back up to the root node, update stats.
+        """
+        node = self  # Starting at the current node.
+        while node is not None:
+            node.visits += 1
+            if node.parent:  # Avoid updating score if we're on the root node.
+                node.value += self.get_reward(node, game_result)
+            node = node.parent
+
+    def sim(self):
+        """
+        Perform simulation and return the best action to take from the root node.
+        """
+        root = self  # Initial node itself is the root.
+        for sim in range(1, self.opts["sim_lim"]):  # TODO: Add a time limit also.
+            node = root.traverse()
+            game_result = node.rollout()
+            node.backpropagate(game_result)
+            yield root
+        self.opt_child = root.get_best_child(0.0)
