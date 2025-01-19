430f97e6c38e51c976016bb5e199a2cac90b0f98
Stephen Lalor <stephen.lalor.work@gmail.com>
2025-01-19 16:47:24 +0000

    Made playouts parallel to generate training games faster.

--- self_play/self_play.py
+++ self_play/self_play.py
@@ -1,5 +1,7 @@
+import concurrent as conc
 import copy
 import logging
+import multiprocessing as mp
 
 import mlflow
 import numpy as np
@@ -9,7 +11,7 @@ from torch.optim import Adam
 from torch.utils.data import DataLoader
 from torchinfo import summary
 
-from mcts.alpha_zero_mcts import SmartMCSTNode
+from mcts.mcst_node import MCSTNode
 from neural_networks.data_prep import get_input_feats
 from neural_networks.tic_tac_toe_net import TicTacToeNet
 from self_play.game_data import GameData, GameDataset
@@ -23,6 +25,7 @@ class TicTacToeTrainer:
     def __init__(self, cfg: dict):
         self.cfg = cfg
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.workers = mp.cpu_count()
         # Set up initial clean game state.
         self.clean_board = TicTacToeBoard(
             TicTacToeBot("p1", 1, "random"), TicTacToeBot("p2", 2, "random")
@@ -36,28 +39,28 @@ class TicTacToeTrainer:
                 {"params": self.model.get_oth_param_grp(), "lr": cfg["nn"]["oth_lr"]},
             ]
         )
-        self.rng = np.random.default_rng()  # For stochastic selection.
+        # MLflow logging.
         self.tot_steps = 0  # Current training step count.
         self.tot_epochs = 0  # Current training epoch count.
 
-    def self_play(self) -> GameData:
-        # TODO: Do I need to use torch.no_grad() here as well?
+    def self_play(self, model: TicTacToeNet, clean_board: TicTacToeBoard, cfg: dict) -> GameData:
         # Board, action probabilities and reward for each turn.
         game_data = GameData()
+        # Concurrent processes need their own RNG object.
+        rng = np.random.default_rng()  # For stochastic selection.
         # Play until termination.
-        board = copy.deepcopy(self.clean_board)
+        board = copy.deepcopy(clean_board)
         while not board.game_result:
             # Begin search from the current position.
-            mct = SmartMCSTNode(None, None, board, self.cfg["mcts"])
-            actions, probs = mct.search(self.model)
+            mct = MCSTNode(None, None, board, cfg["mcts"])
+            actions, probs = mct.search(model)
             # Do stochastic move selection and execute.
-            action = self.rng.choice(actions, p=probs)
+            action = rng.choice(actions, p=probs)
             board.exec_move(tuple(action))
             # Update history.
             game_data.append_turn(board, probs, board.last_player)
-
         # Assign reward for each turn now that result is known.
-        game_data.finalise(board.last_player, board.game_result, self.cfg["mcts"]["rewards"])
+        game_data.finalise(board.last_player, board.game_result, cfg["mcts"]["rewards"])
         return game_data
 
     def train(self, loader: DataLoader):
@@ -104,34 +107,40 @@ class TicTacToeTrainer:
         # Initialise mlflow logging.
         mlflow.set_tracking_uri(self.cfg["exp"]["uri"])
         mlflow.set_experiment(self.cfg["exp"]["name"])
-        with mlflow.start_run():
-            # Initial logging.
-            # TODO: Write as tempfile instead.
-            with open("model_summary.txt", "w", encoding="utf-8") as f:
-                f.write(str(summary(self.model)))
-            mlflow.log_artifact("model_summary.txt")
-            mlflow.log_params(self.cfg)
+        mlflow.start_run()
+        with open("model_summary.txt", "w", encoding="utf-8") as f:
+            f.write(str(summary(self.model)))
+        mlflow.log_artifact("model_summary.txt")
+        mlflow.log_params(self.cfg)
 
-            for cycle in range(self.cfg["self_play"]["cycles"]):
-                # Begin self play cycle.
-                print(f"--------- Cycle {cycle} ---------")
-                game_data = GameDataset()
-                self.model.eval()  # Eval mode as not training yet.
-                for playout in range(self.cfg["self_play"]["playouts"]):
-                    game = self.self_play()
-                    game_data.append_game(game)
+        # Begin self play and training cycle.
+        for cycle in range(self.cfg["self_play"]["cycles"]):
+            # Generate many games with self play.
+            print(f"--------- Cycle {cycle} ---------")
+            game_data = GameDataset()
+            self.model.eval()  # Not training yet.
+            # Play out games concurrently.
+            with conc.futures.ProcessPoolExecutor(max_workers=self.workers) as pool:
+                futures = []
+                for run in range(self.cfg["self_play"]["playouts"]):
+                    futures.append(
+                        pool.submit(self.self_play, self.model, self.clean_board, self.cfg)
+                    )
+                for future in conc.futures.as_completed(futures):
+                    game_data.append_game(future.result())
 
-                # Begin training using self play data.
-                game_data_loader = game_data.to_loader(self.cfg["nn"]["batch_size"])
-                for epoch in range(self.cfg["self_play"]["epochs"]):
-                    print(f"\t--------- Epoch {epoch} ---------")
-                    # Train.
-                    self.model.train()
-                    self.train(game_data_loader)
-                    # Evaluate.
-                    self.model.eval()
-                    self.evaluate(game_data_loader)
-                    self.tot_epochs += 1
+            # Begin training using self play data.
+            game_data_loader = game_data.to_loader(self.cfg["nn"]["batch_size"])
+            for epoch in range(self.cfg["self_play"]["epochs"]):
+                print(f"\tEpoch {epoch}")
+                # Train.
+                self.model.train()
+                self.train(game_data_loader)
+                # Evaluate.
+                self.model.eval()
+                self.evaluate(game_data_loader)
+                self.tot_epochs += 1
 
-                # Save model to MLflow.
-                mlflow.pytorch.log_model(self.model, f"model_cycle_{str(cycle)}")
+            # Save model to MLflow.
+            mlflow.pytorch.log_model(self.model, f"model_cycle_{str(cycle)}")
+        mlflow.end_run()
