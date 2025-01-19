376e4c2ad01fcd0a4eafb35fd6022d42c8217eaf
Stephen Lalor <stephen.lalor.work@gmail.com>
2025-01-19 16:48:22 +0000

    Increased training params in non-testing config.

--- scripts/train.py
+++ scripts/train.py
@@ -8,7 +8,7 @@ from utils import read_config, set_up_logging
 if __name__ == "__main__":
     # Setup.
     set_up_logging()
-    cfg = read_config("test_training_cfg.yaml")
+    cfg = read_config("training_cfg.yaml")  # Choose config script here.
 
     # Train with self play.
     trainer = TicTacToeTrainer(cfg)
