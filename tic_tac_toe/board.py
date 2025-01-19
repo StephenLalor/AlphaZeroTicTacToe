0df5a85a8a9e950ae37dba3433ccce7b847694e5
Stephen Lalor <stephen.lalor.work@gmail.com>
2025-01-19 21:52:38 +0000

    Removed redundant methods and attributes.

--- tic_tac_toe/board.py
+++ tic_tac_toe/board.py
@@ -8,23 +8,17 @@ class TicTacToeException(Exception):
 
 
 class TicTacToeBoard:
-    def __init__(
-        self,
-        p1: TicTacToeBot,
-        p2: TicTacToeBot,
-        last_player: TicTacToeBot = None,
-        next_player: TicTacToeBot = None,
-    ):
+    def __init__(self, p1: TicTacToeBot, p2: TicTacToeBot):
         # Set up 3x3 board and define value move locations.
         self.dim = (3, 3)
-        self.state = np.zeros(self.dim, dtype=np.int8)
-        self.moves = [loc for loc in np.ndindex(self.state.shape)]
-        self.valid_moves = self.moves.copy()
+        self.state = np.zeros(self.dim, dtype=np.int8)  # Game state i.e. the actual board.
+        self.moves = [loc for loc in np.ndindex(self.state.shape)]  # All possible moves.
+        self.valid_moves = self.moves.copy()  # Currently possible moves.
         # Set up players, defaulting to p1 being next to play.
         self.p1 = p1
         self.p2 = p2
-        self.next_player = next_player if next_player else p1
-        self.last_player = last_player if last_player else p2
+        self.next_player = p1
+        self.last_player = p2
         # Track game results and the last executed move on the board.
         self.last_move = None
         self.game_result = None
@@ -37,12 +31,10 @@ class TicTacToeBoard:
         header_msg = f"Board State (moves: {len(self.valid_moves)}, status: {status})"
         return f"{header_msg}\n{formatted_board}"
 
-    def update_board(self, symbol, move_loc):
-        self.state[move_loc] = symbol
-        self.valid_moves.remove(move_loc)
-        return
-
     def update_result(self):
+        """
+        Check if current board state is terminal.
+        """
         # Check each row for a win.
         for i in range(self.dim[0]):
             if self.state[(i, 0)] and np.all(self.state[i] == self.state[(i, 0)]):
@@ -73,28 +65,21 @@ class TicTacToeBoard:
         return
 
     def exec_move(self, move_loc):
+        """
+        Execute move at move_loc for the current player.
+        """
+        # Check game is still in play.
+        if self.game_result:
+            raise TicTacToeException("Game already ended.")
+        # Check move is valid.
+        if move_loc not in self.valid_moves:
+            raise TicTacToeException(f"{move_loc} is not valid move for this board.")
         # Next player makes their move.
-        self.update_board(self.next_player.val, move_loc)
+        self.state[move_loc] = self.next_player.val
+        self.valid_moves.remove(move_loc)
         # Rotate players so player who just played is last_player.
         self.next_player, self.last_player = self.last_player, self.next_player
         self.last_move = move_loc
         # Check if this resulted in a win or draw.
         self.update_result()
         return
-
-
-def assign_reward(player: TicTacToeBot, last_player: TicTacToeBot, res: str, rewards: dict):
-    # Ensure only handled results are passed.
-    allowed_results = {"win", "draw"}
-    if res not in allowed_results:
-        raise TicTacToeException(f"Result {res} not one of {allowed_results}")
-    # Ensure rewards has all required levels.
-    missing_keys = {"win", "draw", "lose"} - set(rewards)
-    if missing_keys:
-        raise TicTacToeException(f"Rewards dictionary missing keys: {missing_keys}")
-    # Determine reward with respect to player.
-    if res == "draw":
-        return rewards["draw"]
-    if res == "win" and player == last_player:
-        return rewards["win"]
-    return rewards["lose"]
