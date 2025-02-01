f57ec0bf4e11409224447a79182f7fdcfb160ddb
Stephen Lalor <stephen.lalor.work@gmail.com>
2025-02-01 00:35:31 +0000

    Improved NN architecture with more complex value head.

--- neural_networks/tic_tac_toe_net.py
+++ neural_networks/tic_tac_toe_net.py
@@ -1,7 +1,7 @@
-from torch import Tensor, nn, softmax
+import torch
 
 
-class ConvBlock(nn.Module):
+class ConvBlock(torch.nn.Module):
     """
     Initial convolution block before the residual blocks. Extracts low level features and smooths
     data to help deep layers discover more complex features. Also transforms data into correct
@@ -10,9 +10,9 @@ class ConvBlock(nn.Module):
 
     def __init__(self, num_in: int, num_out: int):
         super().__init__()
-        self.conv = nn.Conv2d(num_in, num_out, (3, 3), 1, 1)
-        self.batch_norm = nn.BatchNorm2d(num_out)
-        self.relu = nn.ReLU()
+        self.conv = torch.nn.Conv2d(num_in, num_out, kernel_size=3, padding=1)
+        self.batch_norm = torch.nn.BatchNorm2d(num_out)
+        self.relu = torch.nn.ReLU(inplace=False)
 
     def forward(self, x):
         # Low level feature extraction.
@@ -22,7 +22,7 @@ class ConvBlock(nn.Module):
         return x
 
 
-class ResBlock(nn.Module):
+class ResBlock(torch.nn.Module):
     """
     Residual block to allow for skip connection to mitigate vanishing gradient problem in deep
     networks.
@@ -34,12 +34,12 @@ class ResBlock(nn.Module):
 
     def __init__(self, num_in: int, num_out: int):
         super().__init__()
-        self.conv_1 = nn.Conv2d(num_in, num_out, (3, 3), 1, 1)
-        self.batch_norm_1 = nn.BatchNorm2d(num_out)
-        self.relu_1 = nn.ReLU()
-        self.conv_2 = nn.Conv2d(num_in, num_out, (3, 3), 1, 1)
-        self.batch_norm_2 = nn.BatchNorm2d(num_out)
-        self.relu_3 = nn.ReLU()
+        self.conv_1 = torch.nn.Conv2d(num_in, num_out, kernel_size=3, padding=1)
+        self.batch_norm_1 = torch.nn.BatchNorm2d(num_out)
+        self.relu_1 = torch.nn.ReLU(inplace=False)
+        self.conv_2 = torch.nn.Conv2d(num_in, num_out, kernel_size=3, padding=1)
+        self.batch_norm_2 = torch.nn.BatchNorm2d(num_out)
+        self.relu_3 = torch.nn.ReLU(inplace=False)
 
     def forward(self, x):
         # Preserve residual for skip connection.
@@ -52,12 +52,12 @@ class ResBlock(nn.Module):
         x = self.conv_2(x)
         x = self.batch_norm_2(x)
         # Apply skip connection.
-        x += residual
+        x = x + residual
         x = self.relu_3(x)
         return x
 
 
-class PolicyHead(nn.Module):
+class PolicyHead(torch.nn.Module):
     """
     Consumes feature map from residual blocks and returns a vector of probabilities for each
     possible move.
@@ -65,11 +65,11 @@ class PolicyHead(nn.Module):
 
     def __init__(self, num_in: int, num_out: int, num_feats: int):
         super().__init__()
-        self.conv_1 = nn.Conv2d(num_in, num_feats, (3, 3), 1, 1)
-        self.batch_norm_1 = nn.BatchNorm2d(num_feats)
-        self.relu_1 = nn.ReLU()
-        self.flatten_2 = nn.Flatten()
-        self.linear_2 = nn.Linear(num_feats * num_out, num_out)
+        self.conv_1 = torch.nn.Conv2d(num_in, num_feats, kernel_size=3, padding=1)
+        self.batch_norm_1 = torch.nn.BatchNorm2d(num_feats)
+        self.relu_1 = torch.nn.ReLU(inplace=False)
+        self.flatten_2 = torch.nn.Flatten()
+        self.linear_2 = torch.nn.Linear(num_feats * 9, num_out)
 
     def forward(self, x):
         # Feature extraction.
@@ -82,33 +82,33 @@ class PolicyHead(nn.Module):
         return x
 
 
-class ValueHead(nn.Module):
+class ValueHead(torch.nn.Module):
     """
     Consumes feature map from residual blocks and returns a scaler score representing how good the
     current board position is.
     """
 
-    def __init__(self, num_in: int, action_size: int, num_feats: int):
+    def __init__(self, num_in: int, num_out: int, num_feats: int):
         super().__init__()
-        self.conv_1_out_size = num_feats * action_size
-        self.conv1 = nn.Conv2d(num_in, self.conv_1_out_size, (3, 3), 1, 1)
-        self.batch_norm_1 = nn.BatchNorm2d(self.conv_1_out_size)
-        self.relu_1 = nn.ReLU()
-        self.flatten_2_out_size = self.conv_1_out_size * action_size
-        self.flatten_2 = nn.Flatten()
-        self.linear_2 = nn.Linear(self.flatten_2_out_size, num_feats)
-        self.relu_2 = nn.ReLU()
-        self.linear_3 = nn.Linear(num_feats, 1)
-        self.tanh_3 = nn.Tanh()
+        self.conv1 = torch.nn.Conv2d(num_in, num_feats, kernel_size=3, padding=1)
+        self.batch_norm_1 = torch.nn.BatchNorm2d(num_feats)
+        self.relu_1 = torch.nn.ReLU(inplace=False)
+        self.flatten_2 = torch.nn.Flatten()
+        self.linear_2 = torch.nn.Linear(9 * num_feats, 2 * num_feats)
+        self.batch_norm_2 = torch.nn.BatchNorm1d(2 * num_feats)
+        self.relu_2 = torch.nn.ReLU(inplace=False)
+        self.linear_3 = torch.nn.Linear(64, num_out)
+        self.tanh_3 = torch.nn.Tanh()
 
     def forward(self, x):
         # Feature extraction.
         x = self.conv1(x)
         x = self.batch_norm_1(x)
         x = self.relu_1(x)
-        # Feature compression.
+        # Feature expansion.
         x = self.flatten_2(x)
         x = self.linear_2(x)
+        x = self.batch_norm_2(x)
         x = self.relu_2(x)
         # Map compressed features to scalar score in [-1, 1].
         x = self.linear_3(x)
@@ -116,34 +116,46 @@ class ValueHead(nn.Module):
         return x
 
 
-class TicTacToeNet(nn.Module):
+class TicTacToeNet(torch.nn.Module):
     """
     Residual tower consisting of an initial convolution block, then several residual blocks. The
     output of this is passed to 2 separate heads which calculate the policy and state value.
     """
 
-    def __init__(self, input_feats: Tensor, hparams: dict):
+    def __init__(self, hparams: dict):
         super().__init__()
-        self.action_size = input_feats.shape[2] * input_feats.shape[3]
-        self.conv_block = ConvBlock(input_feats.shape[3], hparams["hidden"])
-        self.res_blocks = nn.ModuleList(
-            [ResBlock(hparams["hidden"], hparams["hidden"])] * hparams["res_blocks"]
+        self.action_size = 9
+        # Shared initial convolution block.
+        self.conv_block = ConvBlock(num_in=3, num_out=hparams["hidden"])
+        # Shared core res-net block.
+        self.res_blocks = torch.nn.ModuleList(
+            [ResBlock(hparams["hidden"], hparams["hidden"]) for _ in range(hparams["res_blocks"])]
         )
-        self.policy_head = PolicyHead(hparams["hidden"], self.action_size, hparams["pol_feats"])
-        self.value_head = ValueHead(hparams["hidden"], self.action_size, hparams["val_feats"])
+        # Separate policy prediction subnetwork.
+        self.policy_head = PolicyHead(
+            num_in=hparams["hidden"], num_out=self.action_size, num_feats=hparams["pol_feats"]
+        )
+        # Separate value prediction subnetwork.
+        self.value_head = ValueHead(
+            num_in=hparams["hidden"], num_out=1, num_feats=hparams["val_feats"]
+        )
+        # Send to device and compile for faster training.
+        self = torch.compile(self)
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.to(self.device)
 
-    def parse_output(self, policy: Tensor, value: Tensor) -> tuple[Tensor, float]:
+    def parse_output(self, policy: torch.Tensor, value: torch.Tensor) -> tuple[torch.Tensor, float]:
         """
         Transform the raw policy and value output into actual probabilities.
         """
         if policy.get_device != -1:
-            parsed_policy = softmax(policy, axis=1).squeeze(0).detach().cpu().numpy()
+            parsed_policy = torch.softmax(policy, axis=1).squeeze(0).detach().cpu().numpy()
         else:
             raise NotImplementedError("Parsing for GPU not implemented yet.")
         parsed_value = value.item()
         return parsed_policy, parsed_value
 
-    def forward(self, x: Tensor) -> tuple[Tensor, Tensor]:
+    def forward(self, x: torch.Tensor) -> tuple[torch.Tensor, torch.Tensor]:
         x = self.conv_block(x)
         for res_block in self.res_blocks:
             x = res_block(x)
@@ -151,13 +163,13 @@ class TicTacToeNet(nn.Module):
         value = self.value_head(x)
         return policy, value
 
-    def get_pol_param_grp(self) -> list[Tensor]:
+    def get_pol_param_grp(self) -> list[torch.Tensor]:
         return list(self.policy_head.parameters())
 
-    def get_val_param_grp(self) -> list[Tensor]:
+    def get_val_param_grp(self) -> list[torch.Tensor]:
         return list(self.value_head.parameters())
 
-    def get_oth_param_grp(self) -> list[Tensor]:
+    def get_oth_param_grp(self) -> list[torch.Tensor]:
         oth_params = []
         for name, param in self.named_parameters():
             if "policy_head" not in name and "value_head" not in name:
