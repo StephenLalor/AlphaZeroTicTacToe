0462e20f3f97d2f4cca07782d8bd5a0c213df5fa
Stephen Lalor <stephen.lalor.work@gmail.com>
2025-02-04 14:01:19 +0000

    Changed perspective of reward in game data

--- self_play/game_data.py
+++ self_play/game_data.py
@@ -38,7 +38,8 @@ class GameData:
         """
         # Add reward for each turn.
         for turn in range(len(self.players)):
-            reward = assign_reward(last, self.players[turn], res, rewards_cfg)
+            # Reward negated to represent opponent's perspective.
+            reward = -assign_reward(last, self.players[turn], res, rewards_cfg)
             self.rewards.append(torch.tensor(reward, dtype=torch.float32).unsqueeze(0))
 
         # Finalise data by stacking into tensors.
