Changed PUCT calculation for brute force and fixed tests.

tests/test_brute_force_mcts.py
@@ -2,6 +2,7 @@ import copy
 
 import numpy as np
 import pytest
+from numpy.random import SeedSequence
 
 from mcts.brute_force_mcst_node import BruteMCSTNode
 from tic_tac_toe.board import TicTacToeBoard
@@ -19,8 +20,13 @@ def cfg():
     return copy.deepcopy(read_config("test_training_cfg.yaml"))
 
 
+@pytest.fixture
+def seed_seq():
+    return SeedSequence()
+
+
 @pytest.mark.repeat(3)
-def test_best_move_high_sim_1(clean_board, cfg):
+def test_best_move_high_sim_1(clean_board, cfg, seed_seq):
     """
     Test best move is chosen using high number of simulations.
     """
@@ -31,14 +37,14 @@ def test_best_move_high_sim_1(clean_board, cfg):
     clean_board.exec_move((1, 1))  # O
     # Do simulation.
     cfg["mcts"]["sim_lim"] = 100  # Ensure simulation count is always high.
-    node = BruteMCSTNode(None, clean_board, cfg)
+    node = BruteMCSTNode(None, clean_board, cfg, seed_seq)
     actions, probs = node.search()
     # Check maximum probability move is correct.
     assert (actions[probs.argmax()] == np.array([0, 2])).all()
 
 
 @pytest.mark.repeat(3)
-def test_best_move_high_sim_2(clean_board, cfg):
+def test_best_move_high_sim_2(clean_board, cfg, seed_seq):
     """
     Test best move is chosen using high number of simulations.
     """
@@ -50,14 +56,14 @@ def test_best_move_high_sim_2(clean_board, cfg):
     clean_board.exec_move((2, 0))  # X
     # Do simulation.
     cfg["mcts"]["sim_lim"] = 100  # Ensure simulation count is always high.
-    node = BruteMCSTNode(None, clean_board, cfg)
+    node = BruteMCSTNode(None, clean_board, cfg, seed_seq)
     actions, probs = node.search()
     # Check maximum probability move is correct.
     assert (actions[probs.argmax()] == np.array([1, 2])).all()
 
 
 @pytest.mark.repeat(3)
-def test_best_move_high_sim_3(clean_board, cfg):
+def test_best_move_high_sim_3(clean_board, cfg, seed_seq):
     """
     Test best move is chosen using high number of simulations.
     """
@@ -68,14 +74,14 @@ def test_best_move_high_sim_3(clean_board, cfg):
     clean_board.exec_move((0, 2))  # O
     # Do simulation.
     cfg["mcts"]["sim_lim"] = 100  # Ensure simulation count is always high.
-    node = BruteMCSTNode(None, clean_board, cfg)
+    node = BruteMCSTNode(None, clean_board, cfg, seed_seq)
     actions, probs = node.search()
     # Check maximum probability move is correct.
     assert (actions[probs.argmax()] == np.array([2, 2])).all()
 
 
 @pytest.mark.repeat(3)
-def test_best_move_high_sim_4(clean_board, cfg):
+def test_best_move_high_sim_4(clean_board, cfg, seed_seq):
     """
     Test best move is chosen using high number of simulations.
     """
@@ -89,7 +95,7 @@ def test_best_move_high_sim_4(clean_board, cfg):
     clean_board.exec_move((2, 1))  # X
     # Do simulation.
     cfg["mcts"]["sim_lim"] = 100  # Ensure simulation count is always high.
-    node = BruteMCSTNode(None, clean_board, cfg)
+    node = BruteMCSTNode(None, clean_board, cfg, seed_seq)
     actions, probs = node.search()
     # Check maximum probability move is correct.
     assert (actions[probs.argmax()] == np.array([2, 2])).all()